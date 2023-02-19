0f48d2d5ca5660d12b28562511b61b555b6dc736
Ludvík Prokopec <prokolud@fit.cvut.cz>
2023-02-19 20:49:45 +0100

    add package

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,15 @@
-<main></main>
+<script>
+	import Sortable from './lib/Sortable.svelte';
+
+	let list = [
+		{ id: 1, value: 'A' },
+		{ id: 2, value: 'B' },
+		{ id: 3, value: 'C' },
+	]
+</script>
+
+<main>
+	<Sortable bind:list let:item on:reorder={(e) => console.log(e.detail.source)}>
+		<div>{item.value}</div>
+	</Sortable>
+</main>
